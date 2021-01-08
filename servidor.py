fe333ffcd9f097b2855bfd30727f5a4b31416e45
marcozsouto <marcozsouto@gmail.com>
2021-01-08 13:43:34 -0300

    atualizando servidor

--- servidor.py
+++ servidor.py
@@ -4,6 +4,7 @@ import numpy as np
 import computadorA
 import computadorB
 
+lista = []
 host = 'localhost'
 porta = 50000
 servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -11,26 +12,54 @@ servidor.bind((host, porta))
 servidor.listen()
 print ('aguardando resposta')
 
+def requisicao(target, cliente):    
+    
+    #target mutex
+    if target == computadorA.processo:
+        mutex =  computadorA.mutex
+    elif target == computadorA.processo1:
+        mutex =  computadorA.mutex1
+    elif target == computadorB.processo:
+        mutex =  computadorB.mutex
+    elif target == computadorB.processo1:
+        mutex =  computadorB.mutex1
+    elif target == computadorB.processo2:
+        mutex =  computadorB.mutex2
+
+    #tenta usar a thread desejada 
+    if mutex == 0:
+        requisicao = threading.Thread(target=target, args=(cliente,))
+        requisicao.run()
+        return True
+    #testa as outras
+    elif computadorA.mutex == 0:
+        requisicao = threading.Thread(target=computadorA.processo, args=(cliente,))
+        requisicao.run()
+        return True
+    elif computadorA.mutex1 == 0:
+        requisicao = threading.Thread(target=computadorA.processo1, args=(cliente,))
+        requisicao.run()
+        return True
+    elif computadorB.mutex == 0:
+        requisicao = threading.Thread(target=computadorB.processo, args=(cliente,))
+        requisicao.run()
+        return True
+    elif computadorB.mutex1 == 0:
+        requisicao = threading.Thread(target=computadorB.processo1, args=(cliente,))
+        requisicao.run()
+        return True
+    elif computadorB.mutex2 == 0:
+        requisicao = threading.Thread(target=computadorB.processo2, args=(cliente,))
+        requisicao.run()
+        return True 
+    lista.append(target, cliente)
+    return False
 
 while True:
     cliente, endereço = servidor.accept()
     print('conectado em ', endereço)
-    
-    # mapeamento das Threads
-    requisicao = threading.Thread(target=computadorA.processo, args=(cliente,))
-    requisicao1 = threading.Thread(target=computadorA.processo1, args=(cliente,))
-    requisicao2 = threading.Thread(target=computadorB.processo, args=(cliente,))
-    requisicao3 = threading.Thread(target=computadorB.processo1, args=(cliente,))
-    requisicao4 = threading.Thread(target=computadorB.processo2, args=(cliente,))
-
-
-    if computadorA.mutex == 0:
-        requisicao.run()
-    elif computadorA.mutex1 == 0:
-        requisicao1.run()
-    elif computadorB.mutex == 0:
-        requisicao2.run()
-    elif computadorB.mutex1 == 0:
-        requisicao3.run()
-    elif computadorB.mutex2 == 0:
-        requisicao4.run()
+    #chamada de novo processo 
+    requisicao(computadorA.processo1, cliente)
+    for item in lista:
+        if requisicao(item[0], item[1]) == False:
+            break
